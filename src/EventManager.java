5028cbb0f3d85a70d7a56017e4bf538996da11f5
JCJordan <jordan.j.craig@durham.ac.uk>
2017-01-28 19:28:05 +0000

    Created EventManager and PlayerManager Classes

---
+++ src/EventManager.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+/**
+ * Created by JCJordan on 28/01/2017.
+ */
+public class EventManager {
+
+    private ArrayList<Event> events;
+
+    public EventManager() {
+
+    }
+
+}
---
+++ src/PlayerManager.java
@@ -0,0 +1,12 @@
+/**
+ * Created by JCJordan on 28/01/2017.
+ */
+public class PlayerManager {
+
+    private Player player;
+
+    public PlayerManager(Player player){
+        this.player = player;
+    }
+
+}
