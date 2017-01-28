567bd9f84f7fd70ce15b27bf43907a9947b24764
JCJordan <jordan.j.craig@durham.ac.uk>
2017-01-28 23:54:00 +0000

    Running Game Work

--- src/MainManager.java
+++ src/MainManager.java
@@ -11,17 +11,46 @@ public class MainManager {
 
     public MainManager(){
         //em.readEventList();
-        gm.start();
+        runGame();
     }
 
     public static void main(String args[]){
         new MainManager();
     }
 
+    public void runGame(){
+        gm.start();
+        while(!gm.loaded){}
+        System.out.println("Graphics Loaded");
+        loadMap();
+        long lastTime = System.currentTimeMillis();
+        long currentTime;
+        double delta;
+        while(gm.loaded){
+            currentTime = System.currentTimeMillis();
+            delta = currentTime - lastTime;
+            if(delta > 1000){
+                update();
+                lastTime = System.currentTimeMillis();
+            }
+        }
+        System.out.println("Goodbye! See you next time! :D");
+        //gm.player.x = gm.player.x + 100;
+        //gm.render();
+    }
+
+    public void update(){
+
+    }
+
     public void updateMap (){
         //HERRO!!
         //Test
         //ergregregregrgr
+
+    }
+
+    public void getAction(){
 
     }
 
