f5c27b4ab97993f8ab1e756e47968c836bc7b4ae
calbbi <callumingle@hotmail.com>
2017-01-28 18:26:26 +0000

    Location base

--- src/Location.java
+++ src/Location.java
@@ -2,4 +2,20 @@
  * Created by JCJordan on 28/01/2017.
  */
 public class Location {
+
+    public int x_pos;
+    public int y_pos;
+    public int type;
+    public float defence;
+    public float strength;
+
+    public Location(int x, int y , int t){
+
+        x_pos = x;
+        y_pos = y;
+        type = t;
+        defence = (float)Math.random();
+        strength = (float)Math.random();
+
+    }
 }
