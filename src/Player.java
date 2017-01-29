9523eac1ac9a43277a24fe4792189b8e9a0f368c
SimeonC96 <simeon@localhost.localdomain>
2017-01-29 00:25:16 +0000

    added player

--- src/Player.java
+++ src/Player.java
@@ -2,5 +2,13 @@
  * Created by JCJordan on 28/01/2017.
  */
 public class Player {
+    private String location;
 
+
+    public String getLocation(){
+        return location;
+    }
+    public void setLocation(String location){
+        this.location = location;
+    }
 }
