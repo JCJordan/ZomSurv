4223ea27db796e84f48c95ce5eab813795b994fa
calbbi <callumingle@hotmail.com>
2017-01-29 07:13:05 +0000

    This is as sort of fucking mess

--- src/Player.java
+++ src/Player.java
@@ -5,28 +5,45 @@ import java.util.Scanner;
  * Created by JCJordan on 28/01/2017.
  */
 public class Player {
-    private Location location;
-    private Location position;
+
+    int x_pos;
+    int y_pos;
+    String loc_type;
     private ArrayList<String> inventory;
 
     public Player(){
         inventory = new ArrayList<>();
-        location = null;
     }
 
-    public Location getLocation(){
-        return location;
-    }
-    public void setLocation(Location location){
-        this.location = location;
+    public void set_pos(int x,int y){
+
+        x_pos = x;
+        y_pos = y;
+
     }
 
-    public Location getPosition(){
-        return position;
+    public int get_x_pos(){
+
+        return x_pos;
+
     }
 
-    public void setPosition(Location position){
-        this.position = position;
+    public int get_y_pos(){
+
+        return y_pos;
+
+    }
+
+    public void set_loc_type(String type){
+
+        loc_type = type;
+
+    }
+
+    public String get_loc_type(){
+
+        return loc_type;
+
     }
 
     public ArrayList<String> getInventory(){
