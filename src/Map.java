4d491d7675d7cecd47d44ce40d1de12439860e06
KluvaDa <daniel.kluvanec@durham.ac.uk>
2017-01-29 01:25:46 +0000

    Fixed Map class

--- src/Map.java
+++ src/Map.java
@@ -15,15 +15,15 @@ public class Map{
 
     private int mapWidth;
     private int mapHeight;
-    public int[][] populationDensity;
-    public int[][] infectionDensity;
-    public float[][] accessibility;//all less than 1
-    public ArrayList<ArrayList<Location>> locations = new ArrayList<ArrayList<Location>>(4);;//0:hospital,1:graveyard,2:church,3:Supermarket
-    float zomSpeed = 0.5f;//less than 1
-    float zomTrickle = 0.01f;
-    float popTrickle = 0.02f;
-    float maxSpreadSpeed = 0.2f;//less than 1
-    float popSpeed = 0.6f;//less than 1
+    private int[][] populationDensity;
+    private int[][] infectionDensity;
+    private float[][] accessibility;//all less than 1
+    private ArrayList<ArrayList<Location>> locations = new ArrayList<ArrayList<Location>>(4);;//0:hospital,1:graveyard,2:church,3:Supermarket
+    float ZOM_SPEED = 0.5f;//less than 1
+    float ZOM_TRICKLE = 0.01f;
+    float POP_TRICKLE = 0.02f;
+    float MAX_SPREAD_SPEED = 0.2f;//less than 1
+    float POP_SPEED = 0.6f;//less than 1
 
 
 
@@ -32,18 +32,27 @@ public class Map{
 
         loadAccessibility();
 
-        BufferedImage img = new BufferedImage(mapWidth, mapHeight, BufferedImage.TYPE_INT_ARGB);
+    }
+
+    // testing method
+    public static void main(String [ ] args){
+        Map map = new Map();
+
+        map.addZombies(0,0,10000);
+
+
+        BufferedImage img = new BufferedImage(map.mapWidth, map.mapHeight, BufferedImage.TYPE_INT_ARGB);
         File f = null;
-        int a;
-        int r;
-        int g;
-        int b;
+        int a; // alpha
+        int r; // red
+        int g; // green
+        int b; // blue
 
-        for(int i = 0;i < mapWidth;i++) {
+        for(int i = 0;i < map.mapWidth;i++) {
 
-            for (int j = 0; j < mapHeight; j++) {
+            for (int j = 0; j < map.mapHeight; j++) {
 
-                if(infectionDensity[i][j] > 0){
+                if(map.infectionDensity[i][j] > 0){
 
                     a = 256;
                     r = 256;
@@ -71,23 +80,23 @@ public class Map{
             System.out.println("Error: " + e);
         }
 
-        for(int l = 0; l < 10;l++){
+        for(int l = 0; l < 100;l++){
 
-            incrementTimeHr();
+            map.incrementTime(60);
         }
 
 
 
 
 
-        img = new BufferedImage(mapWidth, mapHeight, BufferedImage.TYPE_INT_ARGB);
+        img = new BufferedImage(map.mapWidth, map.mapHeight, BufferedImage.TYPE_INT_ARGB);
         f = null;
 
-        for(int i = 0;i < mapWidth;i++) {
+        for(int i = 0;i < map.mapWidth;i++) {
 
-            for (int j = 0; j < mapHeight; j++) {
+            for (int j = 0; j < map.mapHeight; j++) {
 
-                if(infectionDensity[i][j] > 0){
+                if(map.infectionDensity[i][j] > 0){
 
                     a = 50;
                     r = 256;
@@ -116,46 +125,33 @@ public class Map{
         }
 
 
+
     }
 
     private void loadAccessibility(){
 
-        BufferedImage image;
-        File sourceimage = new File("src/Graphics/resources/accessibility.png");
+        BufferedImage imageAccessibility;
+        BufferedImage imagePopulation;
+        File sourceImageAccessibility = new File("src/Graphics/resources/accessibility.png");
+        File sourceImagePopulation = new File("src/Graphics/resources/population.png");
         try {
-            image = ImageIO.read(sourceimage);
-            mapWidth = image.getWidth();
-            mapHeight = image.getHeight();
+            imageAccessibility = ImageIO.read(sourceImageAccessibility);
+            imagePopulation = ImageIO.read(sourceImagePopulation);
+            mapWidth = imageAccessibility.getWidth();
+            mapHeight = imageAccessibility.getHeight();
             populationDensity = new int[mapWidth][mapHeight];
             infectionDensity = new int[mapWidth][mapHeight];
             accessibility = new float[mapWidth][mapHeight];
 
+
             for(int i = 0;i < mapWidth;i++) {
 
                 for (int j = 0; j < mapHeight; j++) {
 
-                    accessibility[i][j] =  (image.getRGB(i,j)& 0xFF)/255;
-                    populationDensity[i][j] = (int)(200000*accessibility[i][j]);
+                    accessibility[i][j] =  (imageAccessibility.getRGB(i,j)& 0xFF)/255;
+                    populationDensity[i][j] = (imagePopulation.getRGB(i,j)& 0xFF)*128;
 
                 }
-
-            }
-            boolean test = false;
-            for(int i = 0;i < mapWidth;i++) {
-
-                for (int j = 0; j < mapHeight; j++) {
-
-                    if(accessibility[i][j] == 1.0f && test == false){
-
-                        infectionDensity[i][j] = 10000;
-                        test = true;
-
-
-                    }
-
-
-                }
-
             }
 
         } catch(IOException e) {
@@ -165,13 +161,27 @@ public class Map{
 
     }
 
+    public void addZombies(int startI, int startJ, int startValue){
+        if(startI >= 0 && startJ<=0 && startI < mapHeight && startJ < mapWidth && accessibility[startI][startJ] == 1.0f){
+            infectionDensity[startI][startJ] = startValue;
+        }
+    }
+
+    public int[][] getPopulationDensity(){
+        return populationDensity;
+    }
+
+    public int[][] getInfectionDensity(){
+        return infectionDensity;
+    }
+
 
     private float sigmoid(float x) {
         return (float)(1/( 1 + Math.pow(Math.E,(-1*x))));
     }
 
     //these deal with population spread
-    private void incrementTimePopMin(int[][] tPopulationDensity , int[][] tInfectionDensity) {
+    private void incrementTimePop(int[][] tPopulationDensity, int numMin) {
 
         for(int i = 0;i < mapWidth;i++) {
 
@@ -185,89 +195,32 @@ public class Map{
 
                     int change;
 
-                    if(i + xValues[k] < mapWidth && i + xValues[k] > -1 && j + yValues[k] < mapHeight && j + yValues[k] > -1 && infectionDensity[i + xValues[k]][j + yValues[k]] > infectionDensity[i][j]){
+                    // check if in bounds
+                    if (i + xValues[k] < mapWidth && i + xValues[k] > -1 && j + yValues[k] < mapHeight && j + yValues[k] > -1){
+                        // check if infection density of adjacent is greater than current
+                        if (infectionDensity[i + xValues[k]][j + yValues[k]] > infectionDensity[i][j]){
+                            float g = (infectionDensity[i + xValues[k]][j + yValues[k]] - infectionDensity[i][j])/infectionDensity[i + xValues[k]][j + yValues[k]];
+                            change = (int)((g + POP_TRICKLE)*populationDensity[i + xValues[k]][j + yValues[k]]*POP_SPEED*accessibility[i][j]);
+                        }
+                        else {
+                            change = (int)(POP_TRICKLE*populationDensity[i + xValues[k]][j + yValues[k]]*POP_SPEED*accessibility[i][j]);
+                        }
 
-                        float g = (infectionDensity[i + xValues[k]][j + yValues[k]] - infectionDensity[i][j])/infectionDensity[i + xValues[k]][j + yValues[k]];
-                        change = (int)((g + popTrickle)*populationDensity[i + xValues[k]][j + yValues[k]]*popSpeed*accessibility[i][j]);
-                        if(change/60 > tPopulationDensity[i + xValues[k]][j + yValues[k]]){
+                        if(change*numMin/60 > tPopulationDensity[i + xValues[k]][j + yValues[k]]){
 
                             change = tPopulationDensity[i + xValues[k]][j + yValues[k]];
                         }
-
-                        tPopulationDensity[i + xValues[k]][j + yValues[k]] -= change/60;
-                        tPopulationDensity[i][j] += change/60;
-
-
-                    }else if(i + xValues[k] < mapWidth && i + xValues[k] > -1 && j + yValues[k] < mapHeight && j + yValues[k] > -1){
-
-                        change = (int)(popTrickle*populationDensity[i + xValues[k]][j + yValues[k]]*popSpeed*accessibility[i][j]);
-
-                        if(change/60 > tPopulationDensity[i + xValues[k]][j + yValues[k]]){
-
-                            change = tPopulationDensity[i + xValues[k]][j + yValues[k]];
-                        }
-
-                        tPopulationDensity[i + xValues[k]][j + yValues[k]] -= change/60;
-                        tPopulationDensity[i][j] += change/60;
+                        tPopulationDensity[i + xValues[k]][j + yValues[k]] -= change*numMin/60;
+                        tPopulationDensity[i][j] += change*numMin/60;
                     }
-
-
                 }
-
             }
         }
 
 
     }
-    private void incrementTimePopHr(int[][] tPopulationDensity , int[][] tInfectionDensity) {
 
-        for(int i = 0;i < mapWidth;i++) {
-
-            for (int j = 0; j < mapHeight; j++) {
-
-
-                int[] xValues = {1,1,0,-1,-1,-1,0,1};
-                int[] yValues = {0,-1,-1,-1,0,1,1,1};
-
-                for(int k = 0;k < xValues.length;k++){
-
-                    int change;
-
-                    if(i + xValues[k] < mapWidth && i + xValues[k] > -1 && j + yValues[k] < mapHeight && j + yValues[k] > -1 && infectionDensity[i + xValues[k]][j + yValues[k]] > infectionDensity[i][j]){
-
-                        float g = (infectionDensity[i + xValues[k]][j + yValues[k]] - infectionDensity[i][j])/infectionDensity[i + xValues[k]][j + yValues[k]];
-                        change = (int)((g + popTrickle)*populationDensity[i + xValues[k]][j + yValues[k]]*popSpeed*accessibility[i][j]);
-                        if(change/60 > tPopulationDensity[i + xValues[k]][j + yValues[k]]){
-
-                            change = tPopulationDensity[i + xValues[k]][j + yValues[k]];
-                        }
-
-                        tPopulationDensity[i + xValues[k]][j + yValues[k]] -= change;
-                        tPopulationDensity[i][j] += change;
-
-
-                    }else if(i + xValues[k] < mapWidth && i + xValues[k] > -1 && j + yValues[k] < mapHeight && j + yValues[k] > -1){
-
-                        change = (int)(popTrickle*populationDensity[i + xValues[k]][j + yValues[k]]*popSpeed*accessibility[i][j]);
-                        if(change/60 > tPopulationDensity[i + xValues[k]][j + yValues[k]]){
-
-                            change = tPopulationDensity[i + xValues[k]][j + yValues[k]];
-                        }
-
-                        tPopulationDensity[i + xValues[k]][j + yValues[k]] -= change;
-                        tPopulationDensity[i][j] += change;
-                    }
-
-
-                }
-
-            }
-        }
-
-    }
-
-    //these deal with zombie spread
-    private void incrementTimeInfMin(int[][] tPopulationDensity , int[][] tInfectionDensity) {
+    private void incrementTimeInf(int[][] tPopulationDensity , int[][] tInfectionDensity, int numMin) {
 
         for(int i = 0;i < mapWidth;i++){
 
@@ -276,113 +229,36 @@ public class Map{
                 int[] xValues = {1,1,0,-1,-1,-1,0,1};
                 int[] yValues = {0,-1,-1,-1,0,1,1,1};
 
-                for(int k = 0;k < xValues.length;k++){
-
-                    int change = 0;
-
-                    if(i + xValues[k] < mapWidth && i + xValues[k] > -1 && j + yValues[k] < mapHeight && j + yValues[k] > -1 && populationDensity[i + xValues[k]][j + yValues[k]] > populationDensity[i][j]){
-
-                        float g = (populationDensity[i + xValues[k]][j + yValues[k]] - populationDensity[i][j])/populationDensity[i + xValues[k]][j + yValues[k]];
-                        change = (int)((g + zomTrickle)*infectionDensity[i + xValues[k]][j + yValues[k]]*zomSpeed*accessibility[i][j]);
-
-                        if(change/60 > tInfectionDensity[i + xValues[k]][j + yValues[k]]){
-
-                            change = tInfectionDensity[i + xValues[k]][j + yValues[k]];
-                        }
-
-                        tInfectionDensity[i + xValues[k]][j + yValues[k]] -= change/60;
-                        tInfectionDensity[i][j] += change/60;
-
-
-                    }else if(i + xValues[k] < mapWidth && i + xValues[k] > -1 && j + yValues[k] < mapHeight && j + yValues[k] > -1){
-
-                        change = (int)(zomTrickle*infectionDensity[i + xValues[k]][j + yValues[k]]*zomSpeed*accessibility[i][j]);
-                        if(change/60 > tInfectionDensity[i + xValues[k]][j + yValues[k]]){
-
-                            change = tInfectionDensity[i + xValues[k]][j + yValues[k]];
-                        }
-
-                        tInfectionDensity[i + xValues[k]][j + yValues[k]] -= change/60;
-                        tInfectionDensity[i][j] += change/60;
-                    }
-
-
-                }
-
-                float inf;
-
-                if(Math.abs(populationDensity[i][j]-infectionDensity[i][j]) == 0){
-
-                    inf = 1;
-
-                }else{
-
-                    inf = (float)(1/Math.abs(populationDensity[i][j]-infectionDensity[i][j]));
-                    inf = sigmoid(inf);
-                }
-
-                int h =  (int)(populationDensity[i][j]*inf*maxSpreadSpeed/60);
-
-                if(h > tPopulationDensity[i][j]){
-
-                    h = tPopulationDensity[i][j];
-                }
-
-                if(infectionDensity[i][j] != 0){
-
-                    tPopulationDensity[i][j] -= h;
-                    tInfectionDensity[i][j] += h;
-                }
-
-
-
-            }
-
-        }
-
-    }
-    private void incrementTimeInfHr(int[][] tPopulationDensity , int[][] tInfectionDensity) {
-
-        for(int i = 0;i < mapWidth;i++){
-
-            for(int j = 0;j < mapHeight;j++){
-
-                int[] xValues = {1,1,0,-1,-1,-1,0,1};
-                int[] yValues = {0,-1,-1,-1,0,1,1,1};
-
+                // MOVING INFECTED AROUND
                 for(int k = 0;k < xValues.length;k++){
 
                     int change;
 
-                    if(i + xValues[k] < mapWidth && i + xValues[k] > -1 && j + yValues[k] < mapHeight && j + yValues[k] > -1 && populationDensity[i + xValues[k]][j + yValues[k]] > populationDensity[i][j]){
+                    // check if in bounds
+                    if(i + xValues[k] < mapWidth && i + xValues[k] > -1 && j + yValues[k] < mapHeight && j + yValues[k] > -1){
 
-                        float g = (populationDensity[i + xValues[k]][j + yValues[k]] - populationDensity[i][j])/populationDensity[i + xValues[k]][j + yValues[k]];
-                        change = (int)((g + zomTrickle)*infectionDensity[i + xValues[k]][j + yValues[k]]*zomSpeed*accessibility[i][j]);
-
-                        if(change > infectionDensity[i + xValues[k]][j + yValues[k]]){
-
-                            change = infectionDensity[i + xValues[k]][j + yValues[k]];
+                        // set change
+                        if (populationDensity[i + xValues[k]][j + yValues[k]] < populationDensity[i][j]){ // check if adjacent population is smaller than centre
+                            // relative difference of healthy population between adjacent and centre  (pop healthy adjacent - pop healthy centre / pop healthy adjacent)
+                            float g = (populationDensity[i][j] - populationDensity[i + xValues[k]][j + yValues[k]])/populationDensity[i][j];
+                            change = (int)((g + ZOM_TRICKLE)*infectionDensity[i + xValues[k]][j + yValues[k]]*ZOM_SPEED*accessibility[i][j]);
+                        }
+                        else{
+                            change = (int)(ZOM_TRICKLE*infectionDensity[i + xValues[k]][j + yValues[k]]*ZOM_SPEED*accessibility[i][j]);
                         }
 
-                        infectionDensity[i + xValues[k]][j + yValues[k]] -= change;
-                        infectionDensity[i][j] += change;
-
-
-                    }else if(i + xValues[k] < mapWidth && i + xValues[k] > -1 && j + yValues[k] < mapHeight && j + yValues[k] > -1){
-
-                        change = (int)(zomTrickle*infectionDensity[i + xValues[k]][j + yValues[k]]*zomSpeed*accessibility[i][j]);
-
+                        // make sure non negative
                         if(change > tInfectionDensity[i + xValues[k]][j + yValues[k]]){
-
                             change = tInfectionDensity[i + xValues[k]][j + yValues[k]];
                         }
 
-                        tInfectionDensity[i + xValues[k]][j + yValues[k]] -= change;
-                        tInfectionDensity[i][j] += change;
+                        tInfectionDensity[i + xValues[k]][j + yValues[k]] -= change*numMin/60;
+                        tInfectionDensity[i][j] += change*numMin/60;
                     }
 
                 }
 
+                // INFECTING MORE PEOPLE
                 float inf;
 
                 if(Math.abs(populationDensity[i][j]-infectionDensity[i][j]) == 0){
@@ -395,7 +271,7 @@ public class Map{
                     inf = sigmoid(inf);
                 }
 
-                int h =  (int)(populationDensity[i][j]*inf*maxSpreadSpeed);
+                int h =  (int)(populationDensity[i][j]*inf*MAX_SPREAD_SPEED*numMin/60);
 
                 if(h > tPopulationDensity[i][j]){
 
@@ -408,9 +284,6 @@ public class Map{
                     tInfectionDensity[i][j] += h;
 
                 }
-
-
-
 
 
             }
@@ -420,26 +293,25 @@ public class Map{
     }
 
     //these apply the above functions to move time forward
-    public void incrementTimeMin(){
+    public void incrementTime(int numMin){
+        // deep clone populationDensity and infectionDensity
+        int[][] tPopulationDensity = new int[mapWidth][mapHeight];
+        int[][] tInfectionDensity = new int[mapWidth][mapHeight];
 
-        int[][] tPopulationDensity = populationDensity;
-        int[][] tInfectionDensity = infectionDensity;
-        incrementTimeInfMin(tPopulationDensity,tInfectionDensity);
-        incrementTimePopMin(tPopulationDensity,tInfectionDensity);
+        for(int i = 0;i < mapWidth;i++) {
+            for (int j = 0; j < mapHeight; j++) {
+                tPopulationDensity[i][j] = populationDensity[i][j];
+                tInfectionDensity[i][j] = infectionDensity[i][j];
+            }
+        }
+
+        incrementTimeInf(tPopulationDensity,tInfectionDensity, numMin);
+        incrementTimePop(tPopulationDensity, numMin);
         populationDensity = tPopulationDensity;
         infectionDensity = tInfectionDensity;
 
     }
-    public void incrementTimeHr(){
 
-        int[][] tPopulationDensity = populationDensity;
-        int[][] tInfectionDensity = infectionDensity;
-        incrementTimeInfHr(tPopulationDensity,tInfectionDensity);
-        incrementTimePopHr(tPopulationDensity,tInfectionDensity);
-        populationDensity = tPopulationDensity;
-        infectionDensity = tInfectionDensity;
-
-    }
 
     //this creates and stores a location object
     public void createLocation(int x, int y, int type){
